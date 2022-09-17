00af519609b003755a183404cc13b32b7e3f7767
es-kyy <es-kyy@smilegate.com>
2022-09-17 14:56:08 +0900

    [feature/common] 패키지 추가 및 헤더컴포넌트 샘플

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Nav from "./common/component/nav/Nav";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Nav></Nav>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
