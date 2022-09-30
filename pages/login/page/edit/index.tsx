304bd5ebfa6cedab08ebe4e63f63c3bf0ae37aab
imjuju <imjuuju@gmail.com>
2022-09-30 20:49:17 +0900

    [feature/login] 파일 삭제
     - 불필요한 파일 삭제

--- pages/login/page/edit/index.tsx
+++
@@ -1,9 +0,0 @@
-const Edit = () => {
-  return (
-    <>
-      <h1>Profile 페이지 입니다.</h1>
-    </>
-  );
-};
-
-export default Edit;
--- pages/login/page/profile.tsx
+++
@@ -1,9 +0,0 @@
-const Profile = () => {
-  return (
-    <>
-      <h1>Profile 페이지 입니다.</h1>
-    </>
-  );
-};
-
-export default Profile;
