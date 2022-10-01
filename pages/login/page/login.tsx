bf8faa4662643311bafd77597bf2ea63cef14aea
imjuju <imjuuju@gmail.com>
2022-10-01 11:35:44 +0900

    [feature/login] 회원가입 폼 분리, 로그인 추가
    - 회원가입 폼 컴포넌트 분리
    - 로그인 추가

--- pages/login/page/login.tsx
+++ pages/login/page/login.tsx
@@ -1,39 +1,70 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 
 const login = () => {
+  const [account, setAccount] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [error, setError] = useState('');
+
+  const { email, password } = account;
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAccount({ ...account, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    try {
+      e.preventDefault();
+      const resp = await axios.post('https://api.realworld.io/api/users/login', { user: account });
+      console.log(resp);
+    } catch (err) {
+      setError('Error!');
+    }
+  };
+
   return (
     <div className="auth-page">
       <div className="container page">
         <div className="row">
           <div className="col-md-6 offset-md-3 col-xs-12">
-            <h1 className="text-xs-center">Sign up</h1>
+            <h1 className="text-xs-center">Sign in</h1>
             <p className="text-xs-center">
-              <a href="">Have an account?</a>
+              <a href="">Need an account?</a>
             </p>
 
-            <ul className="error-messages">
-              <li>That email is already taken</li>
-            </ul>
+            {error === '' || (
+              <ul className="error-messages">
+                <li>{error}</li>
+              </ul>
+            )}
 
-            <form>
+            <form onSubmit={onSubmit}>
               <fieldset className="form-group">
                 <input
                   className="form-control form-control-lg"
                   type="text"
-                  placeholder="Your Name"
+                  placeholder="Email"
+                  name="email"
+                  value={email}
+                  onChange={onChange}
                 />
-              </fieldset>
-              <fieldset className="form-group">
-                <input className="form-control form-control-lg" type="text" placeholder="Email" />
               </fieldset>
               <fieldset className="form-group">
                 <input
                   className="form-control form-control-lg"
                   type="password"
                   placeholder="Password"
+                  name="password"
+                  value={password}
+                  onChange={onChange}
                 />
               </fieldset>
-              <button className="btn btn-lg btn-primary pull-xs-right">Sign up</button>
+              <button className="btn btn-lg btn-primary pull-xs-right" type="submit">
+                Sign in
+              </button>
             </form>
           </div>
         </div>
--- pages/login/page/register.tsx
+++ pages/login/page/register.tsx
@@ -1,31 +1,7 @@
-import axios from 'axios';
-import React, { useState } from 'react';
+import React from 'react';
+import RegisterForm from '../component/RegisterForm';
 
 const register = () => {
-  const [account, setAccount] = useState({
-    username: '',
-    email: '',
-    password: '',
-  });
-
-  const [error, setError] = useState('');
-
-  const { username, email, password } = account;
-
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setAccount({ ...account, [e.target.name]: e.target.value });
-  };
-
-  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    try {
-      e.preventDefault();
-      const resp = await axios.post('https://api.realworld.io/api/users', { user: account });
-      console.log(resp);
-    } catch (err) {
-      setError('Error!');
-    }
-  };
-
   return (
     <div className="auth-page">
       <div className="container page">
@@ -35,46 +11,7 @@ const register = () => {
             <p className="text-xs-center">
               <a href="">Have an account?</a>
             </p>
-            {error === '' || (
-              <ul className="error-messages">
-                <li>{error}</li>
-              </ul>
-            )}
-            <form onSubmit={onSubmit}>
-              <fieldset className="form-group">
-                <input
-                  className="form-control form-control-lg"
-                  type="text"
-                  placeholder="Username"
-                  name="username"
-                  value={username}
-                  onChange={onChange}
-                />
-              </fieldset>
-              <fieldset className="form-group">
-                <input
-                  className="form-control form-control-lg"
-                  type="text"
-                  placeholder="Email"
-                  name="email"
-                  value={email}
-                  onChange={onChange}
-                />
-              </fieldset>
-              <fieldset className="form-group">
-                <input
-                  className="form-control form-control-lg"
-                  type="password"
-                  placeholder="Password"
-                  name="password"
-                  value={password}
-                  onChange={onChange}
-                />
-              </fieldset>
-              <button className="btn btn-lg btn-primary pull-xs-right" type="submit">
-                Sign up
-              </button>
-            </form>
+            <RegisterForm></RegisterForm>
           </div>
         </div>
       </div>
