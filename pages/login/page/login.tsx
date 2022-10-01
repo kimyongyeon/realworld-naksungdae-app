f0af68dea5ac123773a6b462611d4b7d8a55ac87
imjuju <imjuuju@gmail.com>
2022-10-01 11:40:43 +0900

    [feature/login] 로그인 폼 분리
    - 로그인 폼 분리

--- pages/login/page/login.tsx
+++ pages/login/page/login.tsx
@@ -1,30 +1,7 @@
-import axios from 'axios';
-import React, { useState } from 'react';
+import React from 'react';
+import LoginForm from '../component/LoginForm';
 
 const login = () => {
-  const [account, setAccount] = useState({
-    email: '',
-    password: '',
-  });
-
-  const [error, setError] = useState('');
-
-  const { email, password } = account;
-
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setAccount({ ...account, [e.target.name]: e.target.value });
-  };
-
-  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    try {
-      e.preventDefault();
-      const resp = await axios.post('https://api.realworld.io/api/users/login', { user: account });
-      console.log(resp);
-    } catch (err) {
-      setError('Error!');
-    }
-  };
-
   return (
     <div className="auth-page">
       <div className="container page">
@@ -33,39 +10,8 @@ const login = () => {
             <h1 className="text-xs-center">Sign in</h1>
             <p className="text-xs-center">
               <a href="">Need an account?</a>
+              <LoginForm></LoginForm>
             </p>
-
-            {error === '' || (
-              <ul className="error-messages">
-                <li>{error}</li>
-              </ul>
-            )}
-
-            <form onSubmit={onSubmit}>
-              <fieldset className="form-group">
-                <input
-                  className="form-control form-control-lg"
-                  type="text"
-                  placeholder="Email"
-                  name="email"
-                  value={email}
-                  onChange={onChange}
-                />
-              </fieldset>
-              <fieldset className="form-group">
-                <input
-                  className="form-control form-control-lg"
-                  type="password"
-                  placeholder="Password"
-                  name="password"
-                  value={password}
-                  onChange={onChange}
-                />
-              </fieldset>
-              <button className="btn btn-lg btn-primary pull-xs-right" type="submit">
-                Sign in
-              </button>
-            </form>
           </div>
         </div>
       </div>
