a8824b4ba120493aa6faf24a3642760d0db4c197
imjuju <imjuuju@gmail.com>
2022-10-28 21:26:19 +0900

    [feature/login] Hydration Error 수정
     - p 태그 삭제

--- pages/login/page/login.tsx
+++ pages/login/page/login.tsx
@@ -8,10 +8,10 @@ const login = () => {
         <div className="row">
           <div className="col-md-6 offset-md-3 col-xs-12">
             <h1 className="text-xs-center">Sign in</h1>
-            <p className="text-xs-center">
+            <div className="text-xs-center">
               <a href="">Need an account?</a>
               <LoginForm></LoginForm>
-            </p>
+            </div>
           </div>
         </div>
       </div>
--- pages/login/page/register.tsx
+++ pages/login/page/register.tsx
@@ -8,9 +8,9 @@ const register = () => {
         <div className="row">
           <div className="col-md-6 offset-md-3 col-xs-12">
             <h1 className="text-xs-center">Sign up</h1>
-            <p className="text-xs-center">
+            <div className="text-xs-center">
               <a href="">Have an account?</a>
-            </p>
+            </div>
             <RegisterForm></RegisterForm>
           </div>
         </div>
