0a2f39c69b0639e9de2cf223d6ebb99aef8c06db
imjuju <imjuuju@gmail.com>
2022-11-04 18:10:19 +0900

    [feature/login] 토큰 loading 추가

--- pages/common/component/nav/Nav.tsx
+++ pages/common/component/nav/Nav.tsx
@@ -1,4 +1,4 @@
-import { getUser, setUser } from '@src/store/authSlice';
+import { getUser, setIsLoaded, setUser } from '@src/store/authSlice';
 import { RootState } from '@src/store/store';
 import Link from 'next/link';
 import { useEffect } from 'react';
@@ -6,12 +6,13 @@ import { useDispatch, useSelector } from 'react-redux';
 
 const Nav = () => {
   const auth = useSelector((state: RootState) => state.auth);
-  const { user, isLogin } = auth;
+  const { user, isLogin, isLoaded } = auth;
   const dispatch = useDispatch();
 
   useEffect(() => {
     const token = sessionStorage.getItem('token');
     if (token === null) {
+      dispatch(setIsLoaded());
       return;
     }
 
@@ -23,52 +24,54 @@ const Nav = () => {
 
   return (
     <>
-      <nav className="navbar navbar-light">
-        <div className="container">
-          <Link className="navbar-brand" href="/">
-            conduit
-          </Link>
-          <ul className="nav navbar-nav pull-xs-right">
-            <li className="nav-item">
-              <Link className="nav-link active" href="/">
-                Home
-              </Link>
-            </li>
-            {!isLogin ? (
-              <>
-                <li className="nav-item">
-                  <Link className="nav-link" href="/login/page/login">
-                    Sign in
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" href="/login/page/register">
-                    Sign up
-                  </Link>
-                </li>
-              </>
-            ) : (
-              <>
-                <li className="nav-item">
-                  <Link className="nav-link" href="">
-                    <i className="ion-compose">&nbsp;New Article</i>
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" href="/login/page/settings">
-                    <i className="ion-gear-a">&nbsp;Settings</i>
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" href="/login/page/profile">
-                    <i>&nbsp;{user.username}</i>
-                  </Link>
-                </li>
-              </>
-            )}
-          </ul>
-        </div>
-      </nav>
+      {isLoaded && (
+        <nav className="navbar navbar-light">
+          <div className="container">
+            <Link className="navbar-brand" href="/">
+              conduit
+            </Link>
+            <ul className="nav navbar-nav pull-xs-right">
+              <li className="nav-item">
+                <Link className="nav-link active" href="/">
+                  Home
+                </Link>
+              </li>
+              {!isLogin ? (
+                <>
+                  <li className="nav-item">
+                    <Link className="nav-link" href="/login/page/login">
+                      Sign in
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" href="/login/page/register">
+                      Sign up
+                    </Link>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li className="nav-item">
+                    <Link className="nav-link" href="">
+                      <i className="ion-compose">&nbsp;New Article</i>
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" href="/login/page/settings">
+                      <i className="ion-gear-a">&nbsp;Settings</i>
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" href="/login/page/profile">
+                      <i>&nbsp;{user.username}</i>
+                    </Link>
+                  </li>
+                </>
+              )}
+            </ul>
+          </div>
+        </nav>
+      )}
     </>
   );
 };
--- store/authSlice.ts
+++ store/authSlice.ts
@@ -13,6 +13,7 @@ export interface User {
 export interface Auth {
   user: User;
   isLogin: boolean;
+  isLoaded: boolean;
 }
 
 export const getUser = async (token: string): Promise<User> => {
@@ -36,15 +37,20 @@ export const authSlice = createSlice({
       image: '',
     },
     isLogin: false,
+    isLoaded: false,
   },
   reducers: {
     setUser: (state: Auth, action: PayloadAction<User>) => {
       sessionStorage.setItem('token', action.payload.token);
       state.user = action.payload;
       state.isLogin = true;
+      state.isLoaded = true;
+    },
+    setIsLoaded: (state: Auth) => {
+      state.isLoaded = true;
     },
   },
 });
 
-export const { setUser } = authSlice.actions;
+export const { setUser, setIsLoaded } = authSlice.actions;
 export default authSlice;
