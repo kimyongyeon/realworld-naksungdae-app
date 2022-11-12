30dfa608aa348ee2cfe2309c9fbeed0efed8431a
imjuju <imjuuju@gmail.com>
2022-11-12 10:11:27 +0900

    [feature/login] 로딩적용

--- pages/login/page/settings.tsx
+++ pages/login/page/settings.tsx
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux';
 
 const settings = () => {
   const auth = useSelector((state: RootState) => state.auth);
-  const { user } = auth;
+  const { user, isLoaded } = auth;
 
   const [profile, setProfile] = useState({
     username: user.username,
@@ -21,69 +21,75 @@ const settings = () => {
   const { username, email, password, bio, image } = profile;
 
   return (
-    <div className="settings-page">
-      <div className="container page">
-        <div className="row">
-          <div className="col-md-6 offset-md-3 col-xs-12">
-            <h1 className="text-xs-center">Your Settings</h1>
-            <form>
-              <fieldset>
-                <fieldset className="form-group">
-                  <input
-                    className="form-control"
-                    type="text"
-                    placeholder="URL of profile picture"
-                    name="image"
-                    value={image}
-                    onChange={onChange}
-                  />
-                </fieldset>
-                <fieldset className="form-group">
-                  <input
-                    className="form-control form-control-lg"
-                    type="text"
-                    placeholder="Your Name"
-                    name="username"
-                    value={username}
-                    onChange={onChange}
-                  />
-                </fieldset>
-                <fieldset className="form-group">
-                  <textarea
-                    className="form-control form-control-lg"
-                    rows={8}
-                    placeholder="Short bio about you"
-                    name="bio"
-                    value={bio}
-                  ></textarea>
-                </fieldset>
-                <fieldset className="form-group">
-                  <input
-                    className="form-control form-control-lg"
-                    type="text"
-                    placeholder="Email"
-                    name="email"
-                    value={email}
-                    onChange={onChange}
-                  />
-                </fieldset>
-                <fieldset className="form-group">
-                  <input
-                    className="form-control form-control-lg"
-                    type="password"
-                    placeholder="Password"
-                    name="password"
-                    value={password}
-                    onChange={onChange}
-                  />
-                </fieldset>
-                <button className="btn btn-lg btn-primary pull-xs-right">Update Settings</button>
-              </fieldset>
-            </form>
+    <>
+      {isLoaded && (
+        <div className="settings-page">
+          <div className="container page">
+            <div className="row">
+              <div className="col-md-6 offset-md-3 col-xs-12">
+                <h1 className="text-xs-center">Your Settings</h1>
+                <form>
+                  <fieldset>
+                    <fieldset className="form-group">
+                      <input
+                        className="form-control"
+                        type="text"
+                        placeholder="URL of profile picture"
+                        name="image"
+                        value={image}
+                        onChange={onChange}
+                      />
+                    </fieldset>
+                    <fieldset className="form-group">
+                      <input
+                        className="form-control form-control-lg"
+                        type="text"
+                        placeholder="Your Name"
+                        name="username"
+                        value={username}
+                        onChange={onChange}
+                      />
+                    </fieldset>
+                    <fieldset className="form-group">
+                      <textarea
+                        className="form-control form-control-lg"
+                        rows={8}
+                        placeholder="Short bio about you"
+                        name="bio"
+                        value={bio}
+                      ></textarea>
+                    </fieldset>
+                    <fieldset className="form-group">
+                      <input
+                        className="form-control form-control-lg"
+                        type="text"
+                        placeholder="Email"
+                        name="email"
+                        value={email}
+                        onChange={onChange}
+                      />
+                    </fieldset>
+                    <fieldset className="form-group">
+                      <input
+                        className="form-control form-control-lg"
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        value={password}
+                        onChange={onChange}
+                      />
+                    </fieldset>
+                    <button className="btn btn-lg btn-primary pull-xs-right">
+                      Update Settings
+                    </button>
+                  </fieldset>
+                </form>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
